40bf8f75f7ad258943c3be9b856fd93c9aa510e4
Oksana Mironova <kq3hip13z@gmail.com>
2023-07-07 23:54:29 +0200

    Create About, Contact, Sign Up and Login pages

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -6,35 +6,35 @@ import burger3 from '../../assets/burger3.png';
 
 const Menu = () => {
 	const addToCartHandler = itemNum => {};
+    const menuCard = [
+		{ src: burger1, price: 200, title: 'Cheese Burger', delay: 0.1 },
+		{ src: burger2, price: 500, title: 'Veg Cheese Burger', delay: 0.5 },
+		{
+			src: burger3,
+			price: 1800,
+			title: 'Cheese Burger with French Fries',
+			delay: 0.8
+		}
+	];
 	return (
 		<section id="menu">
 			<h1>MENU</h1>
 			<div>
 				{' '}
-				<MenuCard
-					itemNum={1}
-					burgerSrc={burger1}
-					price={200}
-					title="Cheese Burger"
-					handler={addToCartHandler}
-					delay={0.1}
-				/>{' '}
-				<MenuCard
-					itemNum={2}
-					burgerSrc={burger2}
-					price={500}
-					title="Veg Cheese Burger"
-					delay={0.5}
-					handler={addToCartHandler}
-				/>{' '}
-				<MenuCard
-					itemNum={3}
-					burgerSrc={burger3}
-					price={1800}
-					title="Cheese Burger with French Fries"
-					delay={0.8}
-					handler={addToCartHandler}
-				/>
+				{menuCard.map((card, i) => (
+					<>
+						{' '}
+						<MenuCard
+							key={i}
+							itemNum={i}
+							burgerSrc={card.src}
+							price={card.price}
+							title={card.title}
+							handler={addToCartHandler}
+							delay={card.delay}
+						/>
+					</>
+				))}
 			</div>{' '}
 		</section>
 	);
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,181 @@
-// Write all the code here
-import React from 'react';
+import React, { useState } from 'react';
+import {
+	MDBContainer,
+	MDBTabs,
+	MDBTabsItem,
+	MDBTabsLink,
+	MDBTabsContent,
+	MDBTabsPane,
+	MDBBtn,
+	MDBIcon,
+	MDBInput,
+	MDBCheckbox
+} from 'mdb-react-ui-kit';
 
-const Login = () => {
+function Login() {
+	const [justifyActive, setJustifyActive] = useState('tab1');
+
+	const icons = ['facebook-f', 'twitter', 'google', 'github'];
+
+	let inputs;
+	if (justifyActive === 'tab1') {
+		inputs = [
+			{ label: 'Email address', id: 'form1', type: 'email' },
+			{ label: 'Password', id: 'form2', type: 'password' }
+		];
+	} else if (justifyActive === 'tab2') {
+		inputs = [
+			{ label: 'Name', id: 'form1', type: 'text' },
+			{ label: 'Username', id: 'form1', type: 'text' },
+			{ label: 'Email', id: 'form1', type: 'email' },
+			{ label: 'Password', id: 'form1', type: 'password' }
+		];
+	}
+	let tabs = [
+		{ tab: 'tab1', text: 'Login ' },
+		{ tab: 'tab2', text: ' Register' }
+	];
+
+	const handleJustifyClick = value => {
+		if (value === justifyActive) {
+			return;
+		}
+		setJustifyActive(value);
+	};
 	return (
-		<>
-			<h1>Login</h1>
-			<div>It is a Login</div>
-		</>
+		<MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+			<MDBTabs
+				pills
+				justify
+				className="mb-3 d-flex flex-row justify-content-between"
+			>
+				{tabs.map((t, i) => (
+					<MDBTabsItem key={i}>
+						<MDBTabsLink
+							onClick={() => handleJustifyClick(t.tab)}
+							active={justifyActive === t.tab}
+						>
+							{t.text}
+						</MDBTabsLink>
+					</MDBTabsItem>
+				))}
+				{/* <MDBTabsItem>
+					{' '}
+					<MDBTabsLink
+						onClick={() => handleJustifyClick('tab1')}
+						active={justifyActive === 'tab1'}
+					>
+						Login{' '}
+					</MDBTabsLink>
+				</MDBTabsItem>{' '}
+				<MDBTabsItem>
+					<MDBTabsLink
+						onClick={() => handleJustifyClick('tab2')}
+						active={justifyActive === 'tab2'}
+					>
+						{' '}
+						Register
+					</MDBTabsLink>{' '}
+				</MDBTabsItem> */}
+			</MDBTabs>
+			<MDBTabsContent>
+				<MDBTabsPane show={justifyActive === 'tab1'}>
+					<div className="text-center mb-3">
+						{' '}
+						<p>Sign in with:</p>
+						<div
+							className="d-flex justify-content-between mx-auto"
+							style={{ width: '40%' }}
+						>
+							{icons.map((ic, i) => (
+								<>
+									<MDBBtn
+										key={i}
+										tag="a"
+										color="none"
+										className="m-1"
+										style={{ color: '#1266f1' }}
+									>
+										<MDBIcon fab icon={ic} size="sm" />
+									</MDBBtn>{' '}
+								</>
+							))}
+						</div>
+						<p className="text-center mt-3">or:</p>{' '}
+					</div>
+					{inputs.map((input, i) => (
+						<>
+							<MDBInput
+								key={i}
+								wrapperClass="mb-4"
+								label={input.label}
+								id={input.id}
+								type={input.type}
+							/>{' '}
+						</>
+					))}
+					<div className="d-flex justify-content-between mx-4 mb-4">
+						{' '}
+						<MDBCheckbox
+							name="flexCheck"
+							value=""
+							id="flexCheckDefault"
+							label="Remember me"
+						/>
+						<a href="!#">Forgot password?</a>{' '}
+					</div>
+					<MDBBtn className="mb-4 w-100">Sign in</MDBBtn>{' '}
+					<p className="text-center">
+						Not a member? <a href="#!">Register</a>{' '}
+					</p>
+				</MDBTabsPane>
+				<MDBTabsPane show={justifyActive === 'tab2'}>
+					{' '}
+					<div className="text-center mb-3">
+						<p>Sign up with:</p>
+						<div
+							className="d-flex justify-content-between mx-auto"
+							style={{ width: '40%' }}
+						>
+							{icons.map((ic, i) => (
+								<>
+									<MDBBtn
+										key={i}
+										tag="a"
+										color="none"
+										className="m-1"
+										style={{ color: '#1266f1' }}
+									>
+										<MDBIcon fab icon={ic} size="sm" />
+									</MDBBtn>{' '}
+								</>
+							))}
+						</div>
+						<p className="text-center mt-3">or:</p>{' '}
+					</div>
+					{inputs.map((input, i) => (
+						<>
+							<MDBInput
+								key={i}
+								wrapperClass="mb-4"
+								label={input.label}
+								id={input.id}
+								type={input.type}
+							/>{' '}
+						</>
+					))}
+					<div className="d-flex justify-content-center mb-4">
+						{' '}
+						<MDBCheckbox
+							name="flexCheck"
+							id="flexCheckDefault"
+							label="I have read and agree to the terms"
+						/>{' '}
+					</div>
+					<MDBBtn className="mb-4 w-100">Sign up</MDBBtn>{' '}
+				</MDBTabsPane>
+			</MDBTabsContent>{' '}
+		</MDBContainer>
 	);
-};
-
+}
 export default Login;
